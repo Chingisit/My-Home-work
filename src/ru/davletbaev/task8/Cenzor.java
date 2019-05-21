adaa1605693f0838d9413786ff2b11c79bdfae9a
Ildar Davletbaev <i.davletbaev.stc@innopolis.university>
2019-05-21 17:13:20 +0300

    task8

---
+++ src/ru/davletbaev/task8/Cenzor.java
@@ -0,0 +1,8 @@
+package ru.davletbaev.task8;
+
+public class Cenzor {
+    public static void main(String[] args) {
+        String Str = "бяка";
+        System.out.println(Str.replace("бяка", "вырезано цензурой"));
+    }
+}
